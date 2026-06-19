5c6111e4af391ef9b77507baba59d5ba51ce152c
Your Name <your_email@example.com>
2026-06-19 15:25:20 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,14 @@ public class Main {
                 } else {
                     System.out.println();
                 }
+            } else if (input.startsWith("type ")) {
+                String command = input.substring(5);
+
+                if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
