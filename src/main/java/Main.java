ac5592c3bf3eb31cf7f2fa47595b23dc51bd3256
Your Name <your_email@example.com>
2026-06-19 15:17:44 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("$ ");
-        String command = scanner.nextLine();
-
-        System.out.println(command + ": command not found");
+        while (true) {
+            System.out.print("$ ");
+            String command = scanner.nextLine();
+            System.out.println(command + ": command not found");
+        }
     }
 }
