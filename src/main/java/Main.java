3d19faa40233b9a8426cad504dcd8f4ef673a44e
Your Name <your_email@example.com>
2026-06-19 15:26:40 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -24,7 +25,23 @@ public class Main {
                 if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
                     System.out.println(command + " is a shell builtin");
                 } else {
-                    System.out.println(command + ": not found");
+                    String pathEnv = System.getenv("PATH");
+                    String[] paths = pathEnv.split(File.pathSeparator);
+                    boolean found = false;
+
+                    for (String path : paths) {
+                        File file = new File(path, command);
+
+                        if (file.exists() && file.canExecute()) {
+                            System.out.println(command + " is " + file.getAbsolutePath());
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println(command + ": not found");
+                    }
                 }
             } else {
                 System.out.println(input + ": command not found");
